5bd42dc65c1297de598ac6c75f5b1b587e6f4d03
acamilojuan <1600@holbertonschool.com>
2020-02-11 23:10:11 +0000

    10-add.c file created

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * add - entry point
+  * @a: variable 1
+  * @b: variable 2
+  * Return: Result of the add
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = (a + b);
+	return (res);
+}
