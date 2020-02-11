6fd526206209d2b41ecc6540816b44a650a16e85
acamilojuan <1600@holbertonschool.com>
2020-02-11 15:31:53 +0000

    6-abs.c file fixed

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _abs - is the function to use the absolute value
+ * @c: is the variable to be checked
+ * Return: is 1 or 0 depends of the condition
+ */
+
+int _abs(int number)
+{
+	if (number < 0)
+	{
+		return (number = number * (-1));
+	}
+	else
+	{
+
+}
