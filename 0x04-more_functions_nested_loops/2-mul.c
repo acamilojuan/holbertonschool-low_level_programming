f1d9f1dbbf8f71a026fb88e3b2e7e419772678b2
acamilojuan <1600@holbertonschool.com>
2020-02-13 17:38:28 +0000

    2-mul.c file created

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * mul - function entry point
+  * @a: variable 1
+  * @b: variable 2
+  * Return: Result of the multiplication
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = (a * b);
+	return (res);
+}
