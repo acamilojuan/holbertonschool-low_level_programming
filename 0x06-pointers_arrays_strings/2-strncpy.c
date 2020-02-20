6ed41ca2c06a9b70c4ec13f496c9380b5895654a
acamilojuan <1600@holbertonschool.com>
2020-02-20 22:53:10 +0000

    2-strncpy.c file created

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strncpy - function
+ * @dest: input 1
+ * @src: input 2
+ * @n: input 3
+ * Return: dest pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != 0; a++)
+	{
+	dest[a] = src[a];
+	}
+	for (; a < n; a++)
+	{
+	dest[a] = '\0';
+	}
+	
+	return (dest);
+}
