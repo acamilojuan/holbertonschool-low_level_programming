4bc874cd57258748762bd966577ef9f7de071fd6
acamilojuan <1600@holbertonschool.com>
2020-02-24 16:26:27 +0000

    0-memset.c file created

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _memset - function
+ * @n: variable related to bytes to be used
+ * @s: input variable pointer 1 that points to 
+ * @b: input variable 2
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+
+	for (a = 0; s[a] <= n; a++)
+	{
+	s[a] = b
+}
