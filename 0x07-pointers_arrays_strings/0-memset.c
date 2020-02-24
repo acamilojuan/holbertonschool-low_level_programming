0f3901b8bc1b4b958c9cd71819694cecb6897c05
acamilojuan <1600@holbertonschool.com>
2020-02-24 17:33:57 +0000

    0-memset.c file created

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,8 +2,9 @@
 /**
  * _memset - function
  * @n: variable related to bytes to be used
- * @s: input variable pointer 1 that points to 
+ * @s: input variable pointer 1 that points to value
  * @b: input variable 2
+ * Return: Pointer to s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
