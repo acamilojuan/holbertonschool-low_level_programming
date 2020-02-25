c94a9d6b3ef77d21476377c25de1b8ea41e27875
acamilojuan <1600@holbertonschool.com>
2020-02-25 19:01:36 +0000

    5-strst.c file created

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strstr - function
+ * @haystack: pointer input 1
+ * @needle: input pointer 2
+ * Return: pointer to first b occurrence
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0, c = a; needle[b] != '\0'; b++, c++)
+		{
+		if (needle[b] != haystack[c] || needle[c] == '\0')
+			break;
+		}
+		if (needle[b] == '\0')
+			return (haystack + a);
+	}
+	return (0);
+}
