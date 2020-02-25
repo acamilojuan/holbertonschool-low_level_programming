5425f685aaa8e2b1d826718e2780f5caa5dd6a09
acamilojuan <1600@holbertonschool.com>
2020-02-25 19:10:41 +0000

    5-strst.c file created

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (b = 0, c = a; needle[b] != '\0'; b++, c++)
 		{
-		if (needle[b] != haystack[c] || needle[c] == '\0')
+		if (needle[b] != haystack[c] || haystack[c] == '\0')
 			break;
 		}
 		if (needle[b] == '\0')
