a5e3e9b5cf99f4e34fe89d91a4d1c01167896fa5
acamilojuan <1600@holbertonschool.com>
2020-02-26 22:02:15 +0000

    3-factorial.c file to be modified

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * factorial - Function
+ * @n: input variable to be used
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	int a = 0;
+
+	if (s[a] != '\0')
+	{
+	a = factorial((void *)(s + 1));
+	a++;
+	}
+	return (a);
+}
