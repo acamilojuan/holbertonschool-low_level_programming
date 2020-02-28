7a57e671599a02e0b117b60af09c9dfcd9dd05ec
acamilojuan <1600@holbertonschool.com>
2020-02-28 19:42:49 +0000

    0-whatsmyname.c file created

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: input argument
+ * @argv: input string to be used
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s", argv[argc]);
+	return (0);
+}
