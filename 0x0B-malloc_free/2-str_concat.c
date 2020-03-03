3130f628eed0147b728a53128a2964309a8c4374
acamilojuan <1600@holbertonschool.com>
2020-03-03 18:25:30 -0500

    2-str_concat.c file created

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *str_concat - function
+ * @s1: variable pointer to be used
+ * @s2: variable pointer2 to be used
+ * Return: Null or pointer to array
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int a, b, c, d, e;
+	char *s3;
+
+	if (*s1 == '\0')
+	{
+		s1 = "";
+	}
+	if (*s2 == '\0')
+	{
+		s2 = "";
+	}
+	for (a = 0; s1[a] != '\0';) /** size of s1[] */
+	{
+	a++;
+	}
+	for (b = 0; s2[b] != '\0';) /** size of s2[] */
+	{
+	b++;
+	}
+	c = a + b;
+	s3 = (char *) malloc((sizeof(char) * c) + 1);
+	if (!s3)
+		return (NULL);
+	for (d = 0; d < a; d++)
+	{
+		s3[d] = s1[d];
+	}
+	for (e = 0; e <= b; e++)
+	{
+		s3[d + e] = s2[e];
+	}
+	return (s3);
+}
