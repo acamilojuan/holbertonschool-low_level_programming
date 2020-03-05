107f457d2245aa98783ce1782826fd5d21fcb930
acamilojuan <1600@holbertonschool.com>
2020-03-05 19:21:28 +0000

    3-array_range.c file created

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *array_range - function
+ * @min: variable to be used
+ * @max: variable 2 to be used
+ * Return: NULL or pointer to the array
+ */
+int *array_range(int min, int max)
+{
+	int *array;
+
+	if (min > max)
+		return (NULL);
+	array = malloc(sizeof(int) * (min + max));
+	if (!array)
+		return (NULL);
+	return (array);
+}
