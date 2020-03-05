3498849a215ac720e9e7c9e4afd1fa613756a37c
acamilojuan <1600@holbertonschool.com>
2020-03-05 17:43:26 +0000

    2-calloc.c file created

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,6 +10,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *str;
+	unsigned int a;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -17,5 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	str = malloc(size * nmemb);
 	if (!str)
 		return (NULL);
+	for (a = 0; str[a] != '\0'; a++)
+		str[a] = 0;
 	return (str);
 }
