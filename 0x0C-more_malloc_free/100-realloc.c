2a811c57b9eeba17c3bfb0d55f4dfaa605e2102b
acamilojuan <1600@holbertonschool.com>
2020-03-05 21:45:38 +0000

    100-realloc.c file created

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_realloc - function
+ * *ptr: pointer input
+ * @old_size: variable 1 to be used
+ * @new_size: variable 2 to be used
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size > old_size)
+		s = malloc(
+	
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+		free(ptr);
+		return (NULL)
+	
+		
+}
