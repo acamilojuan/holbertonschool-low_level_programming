8b610c2f389544d9bffba91bfcbb91a34c2d0932
acamilojuan <1600@holbertonschool.com>
2020-03-05 17:38:18 +0000

    2-calloc.c file created

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_calloc - function
+ * @nmemb: variable to be used
+ * @size: variable 2 to be used
+ * Return: Null or pointer to the new array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *str;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	str = malloc(size * nmemb);
+	if (!str)
+		return (NULL);
+	return (str);
+}
