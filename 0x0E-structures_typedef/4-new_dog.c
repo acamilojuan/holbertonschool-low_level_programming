aababb0d5aae0a764accf1e9c438a2f3512f60cf
acamilojuan <1600@holbertonschool.com>
2020-03-09 21:49:13 +0000

    4-new_dog.c file created

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - function
+ * struct dog - struct
+ * @d: variable pointer to structure
+ * Description: function to print the structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+}
