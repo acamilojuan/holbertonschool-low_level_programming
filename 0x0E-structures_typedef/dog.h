2e122eb2323fdb1239698d5eaf72dde2f4064bd7
acamilojuan <1600@holbertonschool.com>
2020-03-09 18:19:41 +0000

    dog.h file created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog
+#define dog
+/**
+ * struct dog - struct
+ * @name: variable char pointer
+ * @age: variable float
+ * @owner: variable char pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
