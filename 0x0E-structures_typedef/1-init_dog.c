745e2476daa20447eb0556a617f9f5392f3c7369
acamilojuan <1600@holbertonschool.com>
2020-03-09 19:38:18 +0000

    1-init_dog.c file fixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,6 +7,8 @@
  * @name: variable char pointer
  * @age: variable float
  * @owner: variable char pointer
+ * @d: variable pointer to structure
+ * Description: function to initialize the structure
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
