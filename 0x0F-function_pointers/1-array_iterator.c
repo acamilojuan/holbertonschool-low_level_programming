485618158e4b30796c6bbabe463a2b748407b600
acamilojuan <1600@holbertonschool.com>
2020-03-11 18:22:19 +0000

    1-array_iterator.c file fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int a;
 
-	if (action != '\0' && size != '\0')
+	if (action != '\0' && size != '\0' && array != '\0')
 		for (a = 0; a < size; a++)
 			action(array[a]);
 }
