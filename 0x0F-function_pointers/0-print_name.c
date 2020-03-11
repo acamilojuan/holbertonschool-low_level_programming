d5ed398c5f4566caa4d81068e90bb4b8af0da8d4
acamilojuan <1600@holbertonschool.com>
2020-03-11 15:38:12 +0000

    0x0F-function_pointers directory creation

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_name - function
+ * @name: pointer to char
+ * @f: pointer to a function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	
+}
