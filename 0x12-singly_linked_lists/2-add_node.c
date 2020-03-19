7605e02652137f743b08808307f8b4f720d94a0c
acamilojuan <1600@holbertonschool.com>
2020-03-19 20:56:50 +0000

    2-add_node.c file fixed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,9 +10,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	new = malloc(sizeof(list_t));
 	new->str = strdup(str);
 	new->len = _strlen(str);
 	new->next = *head;
