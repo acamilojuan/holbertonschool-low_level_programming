1234276551645f5bc5161f1aa34d06431f659571
acamilojuan <1600@holbertonschool.com>
2020-03-25 03:03:29 +0000

    8-sum_listint.c file fixed

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,10 +8,12 @@ int sum_listint(listint_t *head)
 {
 	int sum;
 
-	while (head != NULL)
+	while (head != '\0')
 	{
 		sum += (*head).n;
 		head = (*head).next;
 	}
 	return (sum);
+	if (head == NULL)
+		return (0);
 }
