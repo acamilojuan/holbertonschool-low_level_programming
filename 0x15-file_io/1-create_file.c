e33bcdc43467b5087d874a8210043fb1eaf481e5
acamilojuan <1600@holbertonschool.com>
2020-04-01 01:30:49 +0000

    1-create_file.c fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
 /**
- * read_textfile - Fuction to read a text file and prints in the stdout.
+ * create_file - Function to create a file.
  * @filename: Pointer to the file to be read.
- * @letters: Variable to know the chars to be read and print.
- * Return: The number of chars read and to print.
+ * @text_content: Pointer to the text content.
+ * Return: -1 if error, 1 if succeeds.
  */
 int create_file(const char *filename, char *text_content)
 {
 	int content, a, wr;
 
-	if (filename == '\0')
-		return (-1);
+	content = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (text_content == '\0')
 		text_content = "";
-	content = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	for (a = 0; text_content[a]; a++)
+	if (content == -1)
+		return (-1);
+	for (a = 0; text_content[a];)
+		a++;
 	wr = write(content, text_content, a);
 	if (wr == -1)
 	{
