77480e29b8e180834afc0c6185e895370e9a8feb
acamilojuan <1600@holbertonschool.com>
2020-04-29 20:45:53 +0000

    8-print_square.c file created in mock interview

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * print_square - Function to print squares of # chars
+ * @size: Input integer to know the size of the square
+ * Return: Always success
+*/
+void print_square(int size)
+{
+	int a, b;
+
+	if (size == 0)
+		_putchar('\n');
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
