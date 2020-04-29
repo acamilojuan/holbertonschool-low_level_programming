10a237720862a292f23e17d71ad6612000d88f49
acamilojuan <1600@holbertonschool.com>
2020-04-29 21:27:06 +0000

    9-fizz_buzz.c file created in mock interview fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,22 @@ int main(void)
 		if (((a % 3) == 0) && ((a % 5) == 0))
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else if ((a % 5) == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else if ((a % 3) == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%d", a);
+		}
+		if (!(a == 100))
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
