5abfef8cc25700f59f42666aeb496ddf61fdb8de
acamilojuan <1600@holbertonschool.com>
2020-04-29 21:08:14 +0000

    9-fizz_buzz.c file created in mock interview

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function to iterate and print
+ * Return: Always zero.
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%i", a);
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
