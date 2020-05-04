889163e18177db0cf89449b0e25cdb9c1a16a9d9
acamilojuan <1600@holbertonschool.com>
2020-05-04 01:30:07 +0000

    7-insert_dnodeint.c file modified

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	(*new).n = n;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	if (idx == dlistint_len(*h))
+	else if (idx == dlistint_len(*h))
 		return (add_dnodeint_end(h, n));
 	else
 	{
