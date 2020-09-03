d72e68c203c8841804bb4aa09801de3df22353ab
acamilojuan <1600@holbertonschool.com>
2020-09-02 21:01:58 -0500

    Task 1 created and tested

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+/**
+ * binary_search - fx to find a value in sorted int array with Binary search algorithm
+ * @array: pointer to the 1st element of the array
+ * @size: size of the array
+ * @value: the value to  be searched
+ * Return: index location
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t first = 0, last = size - 1, mid = 0, index = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (first <= last)
+	{
+		printf("Searching in array: ");
+		for (index = first; index < last; index++)
+			printf("%d, ", array[index]);
+		printf("%d\n", array[index]);
+
+		mid = (last + first) / 2;
+		if (value == array[mid])
+			return (mid);
+
+		else if (value < array[mid])
+			last = mid - 1;
+
+		else if (value > array[mid])
+			first = mid + 1;
+	}
+	return (-1);
+}
